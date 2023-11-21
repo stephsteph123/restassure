536c07d9e9c7fcb5b66b422cbb1bc1a379379ecc
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-11-20 20:59:34 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GetData from './GetData';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GetData/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
